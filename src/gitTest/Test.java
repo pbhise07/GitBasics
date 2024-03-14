41eb6b1731fa03bdf95966e818c3e8479e575e0f
pbhiseo7 <pradips.bhise07@gmail.com>
2024-03-14 13:48:19 +0530

    commiting for first time

---
+++ src/gitTest/Test.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is test1");
+		
+		System.out.println("This is test2");
+		
+		System.out.println("This is test3");
+		
+		
+
+		
+	}
+}
